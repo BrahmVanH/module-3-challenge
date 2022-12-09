c0fcb9fbab6857460b495c6ac026ee35db2172fc
Brahm V <brahmvanh@gmail.com>
2022-12-09 17:09:11 -0500

    WORKING PASSWORD GENERATOR w/ all the fixins except an event listener - keeps breaking

--- Develop/test-script.js
+++ Develop/test-script.js
@@ -1,30 +1,27 @@
-
 var generateBtn = document.querySelector("#generate");
+
+var length = Number(prompt("Enter a password length between 8 and 128")),
+charCase = prompt("Enter uppercase or lowercase to select type-case")
+charType = prompt("Enter a character type: special or numeric"),
+password = generatePassword();
 document.getElementById("password").value = password;
+//document.getElementById('copy-btn').addEventListener('click', copyPassword);
 
-generateBtn.addEventListener("click", generatePassword() {
-  var length = Number(prompt("Enter a password length between 8 and 128"));
-  charCase = prompt("Enter uppercase or lowercase");
-  charType = prompt("Enter special or numeric to use characters");
-  password = generatePassword;}
-
- 
-  function generatePassword() {
+function generatePassword() {
   var charSets = {
-          lowercase: 'abcdefghijklmnopqrstuvwxyz',
-          uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-          numeric: '0123456789',
-          special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-      };
-      var charSet = charSets[charType.toLowerCase()] + charSets[charCase.toLowerCase()];
-      var retVal = "";
-      for (var i = 0; i < length; i++) {
-          retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
-      }
-      return retVal;
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '0123456789',
+    special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  };
+  var charSet = charSets[charType.toLowerCase()] + charSets[charCase.toLowerCase()]  // || charSets.lowercase;
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
-)
-    
+  return retVal;
+}
 
 
+//change "display" element ID to index'd element ID"  ---DONE**
 
